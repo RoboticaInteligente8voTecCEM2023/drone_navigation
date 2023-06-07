0716e7501f2266b5efdcb291f948f1835e840341
AgusDV111 <104184664+AgusDV111@users.noreply.github.com>
2023-06-07 17:40:49 -0600

    Add files via upload

--- scripts/drone_control/src/hover.py
+++ scripts/drone_control/src/hover.py
@@ -33,6 +33,9 @@ class HoverDrone():
 		self.controller_up, self.controller_down = 0.0, 0.0
 		self.controller_roll, self.controller_pitch  = 0.0, 0.0
 		self.controller_yawr, self.controller_yawl = 0.0, 0.0
+		self.yaw = 0.0
+		self.roll = 0.0
+		self.pitch = 0.0
 		self.height = 0.0
 
 		self.desired_alt = 1.5
@@ -51,12 +54,12 @@ class HoverDrone():
 		print("down: " + str(self.controller_down))
 		self.controller_yawl = msg.buttons[4] # rigth rt
 		self.controller_yawr = msg.buttons[5] # left lt
-		# print("yaw right: " + str(self.controller_yawr))
-		# print("yaw left: " + str(self.controller_yawl))
+		print("yaw right: " + str(self.controller_yawr))
+		print("yaw left: " + str(self.controller_yawl))
 		self.controller_roll = msg.axes[3] # right joystick ( left to right )
 		self.controller_pitch = msg.axes[1] # left joystick ( up to down )
-		# print("roll: " + str(self.controller_roll))
-		# print("pitch: " + str(self.controller_pitch))
+		print("roll: " + str(self.controller_roll))
+		print("pitch: " + str(self.controller_pitch))
 
 
 	def altitude_callback(self,msg):
@@ -73,20 +76,33 @@ class HoverDrone():
 	def hover(self):
 
 		self.position_x += self.drone_linear_acc.x * self.dt + ((self.drone_linear_acc.x * (self.dt * self.dt)))
-		print("x: " + str(self.position_x))
+		#print("x: " + str(self.position_x))
 		self.position_y += self.drone_linear_acc.y * self.dt + ((self.drone_linear_acc.y * (self.dt * self.dt)))
-		print("y: " + str(self.position_y))
+		#print("y: " + str(self.position_y))
 		self.position_z = self.height
-		print("z: " + str(self.position_z))
+		#print("z: " + str(self.position_z))
 		print("\n")
 
+		#conditions for LT & RT
 		if self.controller_up < 0:
 			self.desired_alt += abs(self.controller_up)
 		elif self.controller_down < 0:
 			self.desired_alt += self.controller_down
+		#conditions for LB & RB
+		if self.controller_yawl > 0:
+			self.yaw = 2.0
+		elif self.controller_yawr > 0:
+			self.yaw = -2.0
+
+
+
 		#print(self.desired_alt)
 		err = self.desired_alt - self.height
 		self.vel.linear.z = err * self.k
+		self.vel.angular.z = self.yaw
+		# joysticks have ranges from -1 to 1, so it can be sended directly to de /cmd_vel topic 
+		self.vel.linear.y = self.controller_roll * 2 #right joystick
+		self.vel.linear.x = self.controller_pitch * 2 #left joystick
 		self.cmd_vel_pub.publish(self.vel)
 	
 	def main(self):
